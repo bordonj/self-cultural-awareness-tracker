1b8797114103970ad66b174b6d0437b520d46801
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-10-05 17:43:59 -0700

    update style to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,38 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from 'styled-components';
 
-const HelpQueueNavbar = styled.h1`
-  font-size: 24px;
-  text-align: center;
-  color: white;
-  background-color: black;
-`;
-const StyledWrapper = styled.section`
-  background-color: orange;
-`;
+// const HelpQueueNavbar = styled.h1`
+//   font-size: 24px;
+//   text-align: center;
+//   color: white;
+//   background-color: black;
+// `;
+// const StyledWrapper = styled.section`
+//   background-color: orange;
+// `;
 function Navbar(){
   return (
-    <React.Fragment>
-      <StyledWrapper>
-        <HelpQueueNavbar>
-          Help Queue
-        </HelpQueueNavbar>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/journal">Journal</Link>
-          </li>
-          <li>
-            <Link to="/signin">Sign In</Link>
-          </li>
-          <li>
-            <Link to="/lessons">Lessons</Link>
-          </li>
-        </ul>
-      </StyledWrapper>
-    </React.Fragment>
+    <div className="ui secondary pointing menu">
+      <Link to="/" className="link">Home</Link>
+      <Link to="/journal" className="link">Journal</Link>
+      <Link to="/signin" className="link">Sign In</Link>
+      <Link to="/lessons" className="link">Lessons</Link>
+    </div>
   );
 }
 
