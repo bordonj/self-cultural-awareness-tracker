6b0bb6f33a711c15915ec694b4bc736d92b37525
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-10-06 11:54:58 -0700

    add conditional rendering for home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { withFirestore, isLoaded } from 'react-redux-firebase';
 
-const Home = () => {
-  return (
-    <div>
-      Home
-    </div>
-  )
+const Home = (props) => {
+  const auth = props.firebase.auth();
+  console.log('auth.currentUser', auth.currentUser)
+  if(!isLoaded(auth)) {
+    return (
+      <>
+      <h1>Loading...</h1>
+      </>
+    )
+  }
+  if ((isLoaded(auth)) && (!auth.currentUser))  {
+    return (
+      <>
+      <h1>You must be signed in to access home page.</h1>
+      </>
+    )
+  }
+  if ((isLoaded(auth)) && (auth.currentUser != null)) {
+    return (
+      <>
+      <h1>welcome {auth.currentUser.email}</h1>
+      </>
+    )
+  }
 }
 
-export default Home;
+export default withFirestore(Home);
