98f79837520ed025b3f5aa82fc1412c3169a1747
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-10-07 16:39:49 -0700

    add key for div wrapping JournalEntry

--- src/components/EntryList.js
+++ src/components/EntryList.js
@@ -7,9 +7,9 @@ const EntryList = (props) => {
   // console.log('entrylist', props)
 
   const handleChangingSelectedEntry = title => {
-  const entry = props.entryList.find(entry => entry.title === title);
-  setSelectedEntry(entry);
-  props.setSelectedEntry(entry);
+    const entry = props.entryList.find(entry => entry.title === title);
+    setSelectedEntry(entry);
+    props.setSelectedEntry(entry);
   }
   console.log(selectedEntry);
   console.log('props sel', props.selectedEntry)
@@ -17,19 +17,21 @@ const EntryList = (props) => {
   return (
     <>
     {props.entryList.map(entry => {
-      return <JournalEntry
-        whenEntryClicked={handleChangingSelectedEntry}
-        title={entry.title}
-        feeling={entry.feeling}
-        incident={entry.incident} 
-        reaction={entry.reaction}
-        lessonslearned={entry.lessonslearned}
-        reflection={entry.reflection}
-        otherside={entry.otherside}
-        date={entry.date}
-        timeOpen={entry.timeOpen}
-        key={entry.date}
-      />
+      return <div key={entry.title}className="journal-entry">
+        <JournalEntry
+          whenEntryClicked={handleChangingSelectedEntry}
+          title={entry.title}
+          feeling={entry.feeling}
+          incident={entry.incident} 
+          reaction={entry.reaction}
+          lessonslearned={entry.lessonslearned}
+          reflection={entry.reflection}
+          otherside={entry.otherside}
+          date={entry.date}
+          timeOpen={entry.timeOpen}
+          key={Math.random()}
+        />
+      </div>
     })}
     </>
   )
