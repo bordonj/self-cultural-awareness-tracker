88d2297feb6c4f814d70ec34844f42b6c067bf67
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-10-07 07:54:09 -0700

    update navbar when user logs in or out

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import styled from 'styled-components';
+import { isLoaded, sLoaded, withFirestore } from "react-redux-firebase";
 
-// const HelpQueueNavbar = styled.h1`
-//   font-size: 24px;
-//   text-align: center;
-//   color: white;
-//   background-color: black;
-// `;
-// const StyledWrapper = styled.section`
-//   background-color: orange;
-// `;
-function Navbar(){
+
+function Navbar({ firebase, currUser }){
+  const [currentUser, setCurrentUser] = useState();
+  const auth = firebase.auth();
+  console.log(auth.currentUser)
+  console.log(auth);
+  console.log(currUser);
+
+
+  // useEffect(() => {
+  //   // const loggedInUser = localStorage.getItem("user");
+  //   // if (loggedInUser) {
+  //   //   console.log(loggedInUser)
+  //   //   const foundUser = loggedInUser
+  //   //   setCurrentUser(foundUser);
+  //   setCurrentUser(currUser);
+  //   console.log('navbar currentUser', currentUser);
+  // }, [currentUser]);
+
+  useEffect(() => {
+    setInterval(() => {
+      const user = localStorage.getItem("user");
+      setCurrentUser(user);
+      }, [])
+  }, 5000);
+
+  if (typeof currentUser === 'string') {
+  console.log(typeof currentUser)
   return (
-    <div className="ui secondary pointing menu">
-      <Link to="/" className="link">Home</Link>
-      <Link to="/journal" className="link">Journal</Link>
-      <Link to="/signin" className="link">Sign In</Link>
-      <Link to="/lessons" className="link">Lessons</Link>
+    <div className="ui secondary pointing menu" id="navbar">
+    <Link to="/" className="item">Home</Link>
+    <Link to="/journal" className="item">Journal</Link>
+    <Link to="/lessons" className="item">Lessons</Link>
+    <Link user={currentUser} to="/signin" className="item right">{currentUser}</Link>
     </div>
   );
+  } else {
+  return (
+    <>
+    {console.log('not logged in')}
+    <div className="ui secondary pointing menu" id="navbar">
+      <Link to="/" className="item">Home</Link>
+      <Link to="/journal" className="item">Journal</Link>
+      <Link to="/lessons" className="item">Lessons</Link>
+      <Link to="/signin" className="item right">Register/Sign In</Link>
+    </div>
+    {/* <hr/> */}
+    </>
+  );
+  }
 }
 
-export default Navbar;
+export default withFirestore(Navbar);
