b484eb7d84fb2d7f1cae4f206718d5dcd2b12241
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-10-07 09:20:42 -0700

    WIP - App js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useState, useEffect, useLayoutEffect } from "react";
 import Navbar from "./components/Navbar";
 import Journal from "./components/Journal";
 import Signin from "./components/Signin";
 import Lessons from "./components/Lessons";
 import Home from "./components/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
 
-function App(){
+const App = () => {
+  const [user, setUser] = useState();
+
+  console.log('user', user);
+
+  
+  useLayoutEffect(() => {
+  // useEffect(() => {
+    console.log('app js useeffect');
+    setUser(localStorage.getItem("user"));
+    console.log('app', user);
+    // const loggedInUser = localStorage.getItem("user");
+    // console.log('app', loggedInUser);
+    // if (loggedInUser) {
+    //   console.log(loggedInUser)
+    //   const foundUser = loggedInUser
+    //   setUser(foundUser);
+    // }
+  }, []);
+
+  console.log('app user', user)
+
   return ( 
     <Router>
-      <Navbar />
+      <Navbar currUser={user}/>
       <Switch>
         <Route path="/signin">
-          <Signin />
+          {/* <Signin /> */}
+          <Signin onUserFound={setUser} />
         </Route>
         <Route path="/journal">
           <Journal />
@@ -21,7 +44,7 @@ function App(){
           <Lessons />
         </Route>
         <Route path="/">
-          <Home />
+          <Home currUser={user}/>
         </Route>
       </Switch>
     </Router>
