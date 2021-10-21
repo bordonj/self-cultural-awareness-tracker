57c3c6eb3b0e489ceb7dd72800fa01c95ca06f9b
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-10-21 13:41:33 -0700

    correct config  by importing storage and firestore - dont forget env file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,20 +1,22 @@
 import firebase from "firebase/app";
 import 'firebase/auth';
+import 'firebase/storage';
+import 'firebase/firestore';
 
-const firebaseConfig = {
+const firebaseConfig = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID
-};
+});
 
-// initialize firebase
-firebase.initializeApp(firebaseConfig);
+// // initialize firebase
+// firebase.initializeApp(firebaseConfig);
 
 // give authentication instance
-export const auth = firebase.initializeApp (firebaseConfig).auth(); 
+export const auth = firebaseConfig.auth();
 
 // initialize storage and firestore service
 const projectStorage = firebase.storage();
