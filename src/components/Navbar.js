1a322530f78e5c3d2bc0ba96b01a5f70669ea744
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-10-21 16:31:43 -0700

    update Navbar to collapse and fix warnings

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,45 @@
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
-const Navbar = () => {
+const NavBar = () => {
+  const nodeRef = React.useRef(null); 
   const { currentUser, logout } = useAuth();
   console.log(currentUser);
 
   return ( 
-    <nav className="navbar">
-      <h1>Know Myself</h1>
-      {!currentUser && 
-        <>
-          <div className="login">
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
-          </div>
-        </>
-      }
-      {currentUser && 
-        <>
-          <div className="links">
-          <Link to="/">Home</Link>
-          <Link to="/blogs">Blogs</Link>
-          <Link to="/create">New Blog</Link>
-          <Link to="/lessons">Lessons</Link>
-          </div>
-          <div className="login">
-            <Link to="/login" onClick={logout}>Logout</Link>
-            <Link to="/profile">{currentUser.email}</Link>
-          </div>
-        </>
-      }
-      
+    <Navbar collapseOnSelect expand="lg" className="navbar">
+        <Navbar.Brand id="header">Know Myself</Navbar.Brand>
+        <Navbar.Toggle animation="false" aria-controls="responsive-navbar-nav" ref={nodeRef}/>
+        <Navbar.Collapse animation="false" id="responsive-navbar-nav">
+          {!currentUser && 
+            <>
+              <div className="login" ref={nodeRef}>
+                <Link to="/login" className="link">Login</Link>
+                <Link to="/signup" className="link">Sign Up</Link>
+              </div>
+            </>
+          }
+          {currentUser && 
+            <>
+              <div className="links" ref={nodeRef}>
+                <Link to="/" className="link">Home</Link>
+                <Link to="/blogs" className="link">Blogs</Link>
+                <Link to="/create" className="link">New Blog</Link>
+                <Link to="/lessons" className="link">Lessons</Link>
+              </div>
+              <div className="login" ref={nodeRef}>
+                <Link to="/login" onClick={logout} className="link">Logout</Link>
+                <Link to="/profile" className="link">{currentUser.email}</Link>
+              </div>
+            </>
+          }
+        </Navbar.Collapse>
+        
 
-    </nav>
+    </Navbar>
   );
 }
 
-export default Navbar;
+export default NavBar;
