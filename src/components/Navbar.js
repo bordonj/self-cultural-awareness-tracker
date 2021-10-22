790bbc22bb0cb0a379b2a1dd00d7bf413a2a0440
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-10-21 18:54:30 -0700

    clean Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,20 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const NavBar = () => {
-  const nodeRef = React.useRef(null); 
   const { currentUser, logout } = useAuth();
   console.log(currentUser);
 
   return ( 
     <Navbar collapseOnSelect expand="lg" className="navbar">
         <Navbar.Brand id="header">Know Myself</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" ref={nodeRef}/>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           {!currentUser && 
             <>
-              <div className="login" ref={nodeRef}>
+              <div className="login">
                 <Link to="/login" className="link">Login</Link>
                 <Link to="/signup" className="link">Sign Up</Link>
               </div>
@@ -23,13 +22,13 @@ const NavBar = () => {
           }
           {currentUser && 
             <>
-              <div className="links" ref={nodeRef}>
+              <div className="links">
                 <Link to="/" className="link">Home</Link>
                 <Link to="/blogs" className="link">Blogs</Link>
                 <Link to="/create" className="link">New Blog</Link>
                 <Link to="/lessons" className="link">Lessons</Link>
               </div>
-              <div className="login" ref={nodeRef}>
+              <div className="login">
                 <Link to="/login" onClick={logout} className="link">Logout</Link>
                 <Link to="/profile" className="link">{currentUser.email}</Link>
               </div>
