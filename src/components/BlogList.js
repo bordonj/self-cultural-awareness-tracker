adb140e7b52ecdb62ace35b93b1cced8c6cc20ad
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-10-22 16:20:23 -0700

    add message to UI if no blogs yet

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -17,6 +17,7 @@ const BlogList = () => {
   return (
     <div className="blog-list">
       <h1>Blog Entries</h1>
+      { blogs.length === 0 && <h3>None created yet... get started with a <Link to="/create">new blog!</Link></h3>}
       { loading &&
         <div className="spinner-parent"> 
           <Spinner animation="border" role="status" id="spinner">
@@ -29,7 +30,7 @@ const BlogList = () => {
           <div className="blog-preview" key={blog.id}>
             <Link to={`/blogs/${blog.id}`}>
               <p><strong>Date:</strong> { new Date(blog.date.seconds * 1000).toString() }</p>
-              <h2><strong>Title</strong>: { blog.incident }</h2>
+              <h2><strong>Title</strong>: { blog.title }</h2>
             </Link>
           </div>
         )
