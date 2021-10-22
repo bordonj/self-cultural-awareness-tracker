49e929d1f4267e032251d3c1b0759d08457da9f7
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-10-21 18:51:30 -0700

    Add initial content for Home

---
+++ src/components/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+
+function Home() {
+  return (
+    <div className="home">
+      <Card>
+        <Card.Header as="h5">Welcome to "Know Myself!"</Card.Header>
+        <Card.Body>
+          <Card.Title>Introduction</Card.Title>
+          <Card.Text>
+            This is an app to help users better understand themselves, biases, and triggers better. All with the ultimate goal of becoming better people.
+          </Card.Text>
+          <Card.Text>
+            Here, you can keep track of your own reactions to controversial topics.
+          </Card.Text>
+          <Card.Text>
+            There's also access to some lessons (unfinished), but it's a start!
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  )
+}
+
+export default Home;
