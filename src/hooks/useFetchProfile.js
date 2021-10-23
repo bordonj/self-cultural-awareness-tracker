25a9c24342a6535614dabfb42ba6e5fb4e0a7c4c
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-10-22 19:44:41 -0700

    create hook to fetch profile

---
+++ src/hooks/useFetchProfile.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { projectFirestore } from "../firebase/config";
+
+const useFetchProfile = (collection, uid) => {
+  const [pending, setPending] = useState(true);
+  const [doc, setDoc] = useState({})
+
+  useEffect(() => {
+    const docRef = projectFirestore.collection(collection).doc(uid);
+
+    docRef.get().then(doc => {
+      if (doc.exists) {
+        console.log('doc data', doc.data());
+        setPending(false);
+        setDoc(doc.data());
+      } else {
+        console.log('no such doc');
+        setPending(false);
+      }
+    }).catch(err => {
+      console.log('err getting doc', err);
+      setPending(false);
+    });
+
+  }, [collection, uid])
+
+  return { doc, pending };
+}
+
+export default useFetchProfile;
