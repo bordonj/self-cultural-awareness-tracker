1695dba8477f316371085d4e4fe6f990ddde670a
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-10-23 20:14:32 -0700

    update useStorage hook

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,10 +1,7 @@
 import { useState, useEffect } from "react";
-import { useAuth } from "../contexts/AuthContext";
 import { projectStorage } from "../firebase/config";
 
 const useStorage = (file) => {
-  const { currentUser } = useAuth();
-  const { uid } = currentUser;
   const [progress, setProgress] = useState(0);
   const [error, setError] = useState(null);
   const [profilePic, setProfilePic] = useState(null);
@@ -14,7 +11,7 @@ const useStorage = (file) => {
     const storageRef = projectStorage.ref(file.name);
 
     storageRef.put(file).on("state_changed", (snap) => {
-      let percentage = (snap.bytesTransferred / snap.totalBytes) * 100
+      let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
       setProgress(percentage);
     }, err => {
       setError(err)
