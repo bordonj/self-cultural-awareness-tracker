c79d97e361889386b65f336a9ef21fb75497d04a
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-10-23 20:11:21 -0700

    add loader while fetching profile info

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Card, Form, Button, Alert } from "react-bootstrap";
+import { Card, Form, Button, Alert, Spinner } from "react-bootstrap";
 import { useRef } from "react";
 import { useAuth } from "../contexts/AuthContext";
 import { Link, useHistory } from "react-router-dom";
@@ -13,17 +13,24 @@ const Profile = () => {
   const [editProfile, setEditProfile] = useState(false);
   const history = useHistory();
 
-  console.log('profile', doc, pending, uid)
+  console.log('profile', doc, doc.age, doc.name, pending, uid)
   if (editProfile) {
     return (
-      <UpdateProfile setEditProfile={setEditProfile} />
+      <UpdateProfile profile={doc} setEditProfile={setEditProfile} />
     )
   }
   return (  
     <>
-      <img src="https://firebasestorage.googleapis.com/v0/b/know-myself-76d29.appspot.com/o/anon.jpg?alt=media&token=da271531-5d56-41cf-8719-fce913be9803" alt="personal photo" />
-      <h2>{doc.age}</h2>
-      <h2>{doc.name}</h2>
+      { pending &&
+        <div className="spinner-parent"> 
+          <Spinner animation="border" role="status" id="spinner">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </div>
+      }
+      <img src={doc.profilePic} alt="personal photo" style={{width: '300px'}}/>
+      <h2>Age: {doc.age}</h2>
+      <h2>Name: {doc.name}</h2>
       <button onClick={() => setEditProfile(true)}>Update Profile</button>
     </>
   );
