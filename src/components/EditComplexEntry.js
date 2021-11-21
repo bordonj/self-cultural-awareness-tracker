33cd77da76bc86ebbba94b1f9ac3c3e909db8a8a
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-11-20 16:52:21 -0800

    add feelings dropdown for updating blog

--- src/components/EditComplexEntry.js
+++ src/components/EditComplexEntry.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
 import { useHistory } from 'react-router';
 import { useAuth } from '../contexts/AuthContext';
@@ -11,6 +11,8 @@ const EditComplexEntry = ({ blog }) => {
   const [reaction, setReaction] = useState(blog.reaction);
   const [otherside, setOtherside] = useState(blog.otherside);
   const [reflection, setReflection] = useState(blog.reflection);
+  const [otherFeeling, setOtherFeeling] = useState(false);
+  const [oFeeling, setOFeeling] = useState('');
   const [lessonslearned, setLessonslearned] = useState(blog.lessonslearned);
   const [loading, setLoading] = useState(false);
   const history = useHistory();
@@ -18,10 +20,39 @@ const EditComplexEntry = ({ blog }) => {
   const { uid } = currentUser;
   const { id } = useParams(); 
 
+  const feelingsArr = [
+    'Happy',
+    'Surprised',
+    'Bad',
+    'Afraid',
+    'Angry',
+    'Disgusted',
+    'Apprehensive',
+    'Peaceful',
+    'Frustrated',
+    'Resentful',
+  ]
+
+  useEffect(() => {
+    console.log(!feelingsArr.includes(feeling))
+    if (blog.feeling === 'other' || !feelingsArr.includes(feeling)) {
+      setOtherFeeling(true)
+      setFeeling('other');
+      setOFeeling(blog.feeling)
+    } else {
+      setOtherFeeling(false);
+      setOFeeling('');
+    }
+  }, [feeling])
+
   const handleSubmit = async e => {
     e.preventDefault();
-    const newBlog = { title, incident, feeling, reaction, otherside, reflection, lessonslearned, date: blog.date}
-
+    let newBlog;
+    if (feeling === 'other') {
+      newBlog = { title, incident, feeling: oFeeling, reaction, otherside, reflection, lessonslearned, date: blog.date}
+    } else {
+      newBlog = { title, incident, feeling, reaction, otherside, reflection, lessonslearned, date: blog.date}
+    }
     setLoading(true);
 
     await projectFirestore.collection('users').doc(uid).collection('journalEntries').doc(id).set(newBlog);
@@ -43,14 +74,33 @@ const EditComplexEntry = ({ blog }) => {
             value={title}
             onChange={(e) => setTitle(e.target.value)}
           />
-          <label>Feeling:</label>
-          <input 
-            required
+          <label>Feeling: (<a href="https://firebasestorage.googleapis.com/v0/b/know-myself-76d29.appspot.com/o/feelings.png?alt=media&token=2eee0d5f-e9aa-485c-aa67-0f0676dfe5eb" target="_blank">Click for Feelings Wheel</a>)</label>
+          <select
             placeholder="Defensive but remorseful"
             value={feeling}
             onChange={e => setFeeling(e.target.value)}
-            required
-          />
+            required>
+              <option value='Happy'>Happy</option>
+              <option value='Surprised'>Surprised</option>
+              <option value='Bad'>Bad</option>
+              <option value='Afraid'>Afraid</option>
+              <option value='Angry'>Angry</option>
+              <option value='Disgusted'>Disgusted</option>
+              <option value='Apprehensive'>Apprehensive</option>
+              <option value='Peaceful'>Peaceful</option>
+              <option value='Frustrated'>Frustrated</option>
+              <option value='Resentful'>Resentful</option>
+              <option value='other'>Other...</option>
+          </select>
+          {otherFeeling && (
+            <input 
+              placeholder="Defensive but remorseful"
+              value={oFeeling}
+              onChange={e => setOFeeling(e.target.value)}
+              required
+            />
+            )
+          }
           <label>Incident:</label>
           <input
             required
@@ -86,8 +136,8 @@ const EditComplexEntry = ({ blog }) => {
             value={lessonslearned}
             onChange={e => setLessonslearned(e.target.value)}
           />
-          { !loading && <button>Add Blog</button>}
-          { loading && <button disabled>Adding blog..</button>}
+          { !loading && <button>Update Blog</button>}
+          { loading && <button disabled>Updating blog..</button>}
         </form>
       </div>
     </>
--- src/components/EditSimpleEntry.js
+++ src/components/EditSimpleEntry.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
 import { useHistory } from 'react-router';
 import { useAuth } from '../contexts/AuthContext';
@@ -8,15 +8,46 @@ const EditSimpleEntry = ({ blog }) => {
   const [title, setTitle] = useState(blog.title);
   const [feeling, setFeeling] = useState(blog.feeling);
   const [reflection, setReflection] = useState(blog.reflection);
+  const [otherFeeling, setOtherFeeling] = useState(false);
+  const [oFeeling, setOFeeling] = useState('');
   const [loading, setLoading] = useState(false);
   const history = useHistory();
   const { currentUser } = useAuth();
   const { uid } = currentUser;
   const { id } = useParams(); 
 
+  const feelingsArr = [
+    'Happy',
+    'Surprised',
+    'Bad',
+    'Afraid',
+    'Angry',
+    'Disgusted',
+    'Apprehensive',
+    'Peaceful',
+    'Frustrated',
+    'Resentful',
+  ];
+
+  useEffect(() => {
+    if (blog.feeling === 'other' || !feelingsArr.includes(feeling)) {
+      setOtherFeeling(true)
+      setFeeling('other');
+      setOFeeling(blog.feeling)
+    } else {
+      setOtherFeeling(false);
+      setOFeeling('');
+    }
+  }, [feeling])
+
   const handleSubmit = async e => {
     e.preventDefault();
-    const newBlog = { title, feeling, reflection, date: blog.date}
+    let newBlog;
+    if (feeling === 'other') {
+      newBlog = { title, feeling: oFeeling, reflection, date: blog.date}
+    } else {
+      newBlog = { title, feeling, reflection, date: blog.date}
+    }
 
     setLoading(true);
 
@@ -38,13 +69,33 @@ const EditSimpleEntry = ({ blog }) => {
             value={title}
             onChange={(e) => setTitle(e.target.value)}
           />
-          <label>Feeling:</label>
-          <input 
+          <label>Feeling: (<a href="https://firebasestorage.googleapis.com/v0/b/know-myself-76d29.appspot.com/o/feelings.png?alt=media&token=2eee0d5f-e9aa-485c-aa67-0f0676dfe5eb" target="_blank">Click for Feelings Wheel</a>)</label>
+          <select
             placeholder="Defensive but remorseful"
             value={feeling}
             onChange={e => setFeeling(e.target.value)}
-            required
-          />
+            required>
+              <option value='Happy'>Happy</option>
+              <option value='Surprised'>Surprised</option>
+              <option value='Bad'>Bad</option>
+              <option value='Afraid'>Afraid</option>
+              <option value='Angry'>Angry</option>
+              <option value='Disgusted'>Disgusted</option>
+              <option value='Apprehensive'>Apprehensive</option>
+              <option value='Peaceful'>Peaceful</option>
+              <option value='Frustrated'>Frustrated</option>
+              <option value='Resentful'>Resentful</option>
+              <option value='other'>Other...</option>
+          </select>
+          {otherFeeling && (
+            <input 
+              placeholder="Defensive but remorseful"
+              value={oFeeling}
+              onChange={e => setOFeeling(e.target.value)}
+              required
+            />
+            )
+          }
           <label>Reflection:</label>
           <textarea
             style={{height: '150px'}}
@@ -52,8 +103,8 @@ const EditSimpleEntry = ({ blog }) => {
             value={reflection}
             onChange={e => setReflection(e.target.value)}
           />
-          { !loading && <button>Add Blog</button>}
-          { loading && <button disabled>Adding blog..</button>}
+          { !loading && <button>Update Blog</button>}
+          { loading && <button disabled>Updating blog..</button>}
         </form>
       </div>
     </>
